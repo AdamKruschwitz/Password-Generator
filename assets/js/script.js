f4407906f0438ed79f39a70e9c1b5bc0f1649402
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-09-21 15:52:54 -0700

    added input js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,20 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Student Code
+var generatePassword = function () {
+  // Get the password length from the user, between 8-128 inclusive
+  var passwordLength;
+  do {
+    passwordLength = prompt("password length (8-128");
+    // By default prompt returns a string, this converts into a number
+    passwordLength = parseInt(passwordLength);
+  } while(passwordLength < 8 || passwordLength > 129);
+
+  //Get password parameters
+  var useLowercase = confirm("Should the password use lowercase characters?");
+  var useUppercase = confirm("Should the password use Uppercase characters?");
+  var useNumeric = confirm("Should the password use numbers?");
+  var useSpecial = confirm("Should the password use special characters?");
+
+}
